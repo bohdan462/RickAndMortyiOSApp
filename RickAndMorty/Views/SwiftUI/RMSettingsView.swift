8ed64d9610839e0859fce77bb9f149d7b9d6c9ba
Bohdan Tkachenko <techbohdan@gmail.com>
2023-03-30 14:05:46 -0500

    Added SwiftUI for Settings

--- RickAndMorty/Views/SwiftUI/RMSettingsView.swift
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -8,18 +8,42 @@
 import SwiftUI
 
 struct RMSettingsView: View {
+    let viewModel: RMSettingsViewViewModel
     
     init(viewModel: RMSettingsViewViewModel) {
-        <#statements#>
+        self.viewModel = viewModel
     }
     
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+            
+        }
     }
 }
 
 struct RMSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        RMSettingsView()
+        RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+        })))
     }
 }
